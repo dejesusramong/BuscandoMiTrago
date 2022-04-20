ab69bdcc1a3a54fd65d290732f0f16e4c103195b
dejesusramong <dejesusramong@gmail.com>
2022-04-20 15:43:46 -0700

    Add project files.

---
+++ BuscandoMiTrago/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace BuscandoMiTrago.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
